32bb86f1ef4724f910a4d30e9857702618d231ad
StacyFranco <stacyfranco@hotmail.com>
2022-09-11 23:46:27 -0300

    inicio da aplicacao de tratamento de imagem( já salva avatar falta publicação)

---
+++ pages/api/usuario.ts
@@ -0,0 +1,8 @@
+import {NextApiRequest,NextApiResponse} from 'next';
+import {validarTokenJWT} from '../../middlewares/validarTokenJWT';
+
+
+const usuarioEndpoint = (req : NextApiRequest, res : NextApiResponse) =>{
+    return res.status(200).json('Usuario autenticado com sucesso');
+}
+export default validarTokenJWT(usuarioEndpoint);
