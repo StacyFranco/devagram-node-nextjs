a9e4cf09a5ca4a694e3f6285e9ef3ee02a432878
StacyFranco <stacyfranco@hotmail.com>
2023-08-14 12:05:53 -0300

    pesquisa de usuario por nome e mail

---
+++ pages/api/pesquisa.ts
@@ -0,0 +1,28 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import type { RespostaPadraoMsg } from "../../types/RespostaPadraoMsg";
+import { conectarMongoDB } from "../../middlewares/conectaMongoDB";
+import { validarTokenJWT } from "../../middlewares/validarTokenJWT";
+import { UsuarioModel } from "../../models/UsuarioModel";
+
+const pesquisaEndpoint = async ( req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg | any[]>) =>{
+    try {
+        if(req.method === 'GET'){
+            const { filtro } = req.query;
+
+            if(!filtro || filtro.length < 2){
+                return res.status(400).json({erro : 'Favor informar pelo menos 2 caracteres para a busca'})        
+            }
+
+            const usuariosEncontrados = await UsuarioModel.find({
+                //
+                $or: [{nome : {$regex : filtro, $options :'i'}},{email : {$regex : filtro, $options :'i'}}]
+            });
+            return res.status(200).json(usuariosEncontrados);
+        }
+        return res.status(405).json({erro : 'Método informado não é valido'})
+    }catch(e){
+        console.log(e);
+        return res.status(500).json({erro : 'Não foi possivel buscar usuarios'})
+    }
+}
+export default validarTokenJWT(conectarMongoDB(pesquisaEndpoint));
