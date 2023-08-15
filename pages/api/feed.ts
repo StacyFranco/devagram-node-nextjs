95d71205dcf8c2760171f67305a5eb56b33fe073
StacyFranco <stacyfranco@hotmail.com>
2023-08-14 22:27:23 -0300

    feed home + contagem publicacoes realizadas

--- pages/api/feed.ts
+++ pages/api/feed.ts
@@ -4,6 +4,8 @@ import {validarTokenJWT} from '../../middlewares/validarTokenJWT';
 import { conectarMongoDB } from '../../middlewares/conectaMongoDB';
 import { UsuarioModel } from '../../models/UsuarioModel';
 import { PublicacaoModel } from '../../models/PublicacaoModel';
+import { SeguidorModel } from '../../models/SeguidorModel';
+import publicacao from './publicacao';
 
 const feedEndpoint = async (req : NextApiRequest, res : NextApiResponse < RespostaPadraoMsg | any >) => {
     try{
@@ -19,6 +21,39 @@ const feedEndpoint = async (req : NextApiRequest, res : NextApiResponse < Respos
                     .find({idUsuario : usuario._id})
                     .sort({data : -1});
                 return res.status(200).json(publicacoes);
+            }else{
+                const {userId} = req.query;
+                const usuarioLogado = await UsuarioModel.findById(userId);
+                if (!usuarioLogado){
+                    return res.status(400).json({erro: 'Usuario não encontrado' })
+                }
+
+                const seguidores = await SeguidorModel.find({usuarioId : usuarioLogado._id});
+                const seguidoresIds = seguidores.map(s => s.usuarioSeguidoId)
+
+                const publicacoes = await PublicacaoModel.find({
+                    $or: [
+                        {idUsuario : usuarioLogado._id},
+                        {idUsuario : seguidoresIds}
+                    ]
+                })
+                .sort({data : -1}); 
+
+                const result = [];
+                for(const publicacao of publicacoes){
+                    
+                    const usuarioDaPublicacao = await UsuarioModel.findById(publicacao.idUsuario);
+                    
+
+                    if(usuarioDaPublicacao){
+                        const final = {...publicacao._doc, usuario : {
+                            nome : usuarioDaPublicacao.nome,
+                            avatar: usuarioDaPublicacao.avatar,
+                        }}
+                        result.push(final);
+                    }
+                }
+                return res.status(200).json(result);
             }
             
         }
