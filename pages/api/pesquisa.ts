714224122c3dbebbecb18c091f42a23762b4af59
StacyFranco <stacyfranco@hotmail.com>
2023-08-25 14:05:01 -0300

    melhoria da pesquisa

--- pages/api/pesquisa.ts
+++ pages/api/pesquisa.ts
@@ -15,73 +15,60 @@ const pesquisaEndpoint = async (req: NextApiRequest, res: NextApiResponse<Respos
                     return res.status(400).json({ erro: 'Usuario não encontrado' })
                 }
                 usuariosEncontrado.senha = null;
+                const usuarioEncontrado = {
+                    _id: usuariosEncontrado._id,
+                    nome: usuariosEncontrado.nome,
+                    email: usuariosEncontrado.email,
+                    senha: usuariosEncontrado.senha,
+                    avatar: usuariosEncontrado.avatar,
+                    seguidores: usuariosEncontrado.seguidores,
+                    seguindo: usuariosEncontrado.seguindo,
+                    publicacoes: usuariosEncontrado.publicacoes,
+                    segueEsseUsuario: false,
+
+                }
 
                 // Para enviar a informção se eu já sigo ou não esse usuario:
-                const { userId, id } = req?.query;
+                const { userId } = req?.query;
 
                 const usuarioLogado = await UsuarioModel.findById(userId);
-                if (!usuarioLogado) {   
+                if (!usuarioLogado) {
                     return res.status(400).json({ erro: 'Usuario logado não encontrado' });
                 }
-                const usuarioPesquisado = await UsuarioModel.findById(id);
-                if (!usuarioPesquisado) {
-                    return res.status(400).json({ erro: 'Usuario a ser Seguido não encontrado' });
-                }
-                const euJaSigoEsseUsuario = await SeguidorModel.find({ usuarioId: usuarioLogado._id, usuarioSeguidoId: usuarioPesquisado._id });
+
+                const euJaSigoEsseUsuario = await SeguidorModel.find({ usuarioId: usuarioLogado._id, usuarioSeguidoId: usuarioEncontrado._id });
 
                 // tem alguma forma de fazer isso mais facilmente? pq não consigo adicionar ao objeto?
                 if (euJaSigoEsseUsuario && euJaSigoEsseUsuario.length > 0) {
-                    const usuariosEncontradoN = {
-                        _id:usuariosEncontrado._id,
-                        nome: usuariosEncontrado.nome,
-                        email:usuariosEncontrado.email,
-                        senha: usuariosEncontrado.senha,
-                        avatar: usuariosEncontrado.avatar,
-                        seguidores: usuariosEncontrado.seguidores,
-                        seguindo: usuariosEncontrado.seguindo,
-                        publicacoes: usuariosEncontrado.publicacoes,
-                        segueEsseUsuario:true,
-
+                   
+                    usuarioEncontrado.segueEsseUsuario = true;
+                   
+                    return res.status(200).json(usuarioEncontrado);
                 }
                 
-                return res.status(200).json(usuariosEncontradoN);
+                return res.status(200).json(usuarioEncontrado);
 
-                
+            } else {
+                const { filtro } = req.query;
+
+                if (!filtro || filtro.length < 2) {
+                    return res.status(400).json({ erro: 'Favor informar pelo menos 2 caracteres para a busca' })
+                }
+
+                const usuariosEncontrados = await UsuarioModel.find({
+                    //
+                    $or: [{ nome: { $regex: filtro, $options: 'i' } }, { email: { $regex: filtro, $options: 'i' } }]
+                });
+
+                usuariosEncontrados.forEach(user => user.senha = null);
+                return res.status(200).json(usuariosEncontrados);
             }
 
-            const usuariosEncontradoN = {
-                _id:usuariosEncontrado._id,
-                nome: usuariosEncontrado.nome,
-                email:usuariosEncontrado.email,
-                senha: usuariosEncontrado.senha,
-                avatar: usuariosEncontrado.avatar,
-                seguidores: usuariosEncontrado.seguidores,
-                seguindo: usuariosEncontrado.seguindo,
-                publicacoes: usuariosEncontrado.publicacoes,
-                segueEsseUsuario:false,
-            }
-            return res.status(200).json(usuariosEncontradoN);
-        } else {
-            const { filtro } = req.query;
-
-            if (!filtro || filtro.length < 2) {
-                return res.status(400).json({ erro: 'Favor informar pelo menos 2 caracteres para a busca' })
-            }
-
-            const usuariosEncontrados = await UsuarioModel.find({
-                //
-                $or: [{ nome: { $regex: filtro, $options: 'i' } }, { email: { $regex: filtro, $options: 'i' } }]
-            });
-
-            usuariosEncontrados.forEach(user => user.senha = null);
-            return res.status(200).json(usuariosEncontrados);
         }
-
-    }
         return res.status(405).json({ erro: 'Método informado não é valido' })
-} catch (e) {
-    console.log(e);
-    return res.status(500).json({ erro: 'Não foi possivel buscar usuarios' })
-}
+    } catch (e) {
+        console.log(e);
+        return res.status(500).json({ erro: 'Não foi possivel buscar usuarios' })
+    }
 }
 export default politicaCORS(validarTokenJWT(conectarMongoDB(pesquisaEndpoint)));
